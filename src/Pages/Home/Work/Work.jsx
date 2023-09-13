bd762e0c0ef8c056480d88fcaa743563893e4990
OliUllah1 <oliullah915@gmail.com>
2023-09-13 17:01:15 +0600

    work section done

--- src/Pages/Home/Work/Work.jsx
+++ src/Pages/Home/Work/Work.jsx
@@ -7,6 +7,22 @@ import idea from '../../../assets/images/work/idea.png';
 import medkit from '../../../assets/images/work/medkit.png';
 import strategy from '../../../assets/images/work/strategy.png';
 import target from '../../../assets/images/work/target.png';
+import Marquee from "react-fast-marquee";
+import logo1 from '../../../assets/images/work/logo-layer1.png'
+import logo2 from '../../../assets/images/work/logo-layer2.png'
+import logo3 from '../../../assets/images/work/logo-layer3.png'
+import logo4 from '../../../assets/images/work/logo-layer4.png'
+import logo5 from '../../../assets/images/work/logo-layer5.png'
+import logo6 from '../../../assets/images/work/logo-layer6.png'
+import logo7 from '../../../assets/images/work/logo-layer7.png'
+import logo8 from '../../../assets/images/work/logo-layer8.png'
+import logo9 from '../../../assets/images/work/logo-layer9.png'
+import logo10 from '../../../assets/images/work/logo-layer10.png'
+import logo11 from '../../../assets/images/work/logo-layer11.png'
+import logo12 from '../../../assets/images/work/logo-layer12.png'
+import heart from '../../../assets/images/work/heart_beat_graph_dark.svg'
+
+
 const Work = () => {
     return (
         <>
@@ -39,6 +55,83 @@ const Work = () => {
                 <div className='bg-white shadow-lg py-14 rounded-md'>
                     <img className='w-20 mx-auto' src={target} alt="" />
                 </div>
+            </div>
+            <div className='pt-5 pb-10'>
+                <div className='flex mb-10 items-center justify-center text-[#323f7c] text-2xl font-bold font-koho'>
+                    <img src={heart} alt="" />
+                    <h5>What Technology We Are Using For Our Valued Customers</h5>
+                    <img src={heart} alt="" />
+                </div>
+                <Marquee className='mb-10'>
+                    <div className='flex gap-5'>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                        <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo1} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo2} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo3} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo4} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo5} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo6} alt="" />
+                        </div>
+                    </div>
+                    
+                    </div>
+                </Marquee>
+                <Marquee direction={"right"}>
+                    <div className='flex gap-5'>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo7} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo8} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo9} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo10} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo11} alt="" />
+                        </div>
+                    </div>
+                    <div className='bg-[#f2fff7] px-6 border-t border-b rounded-sm flex items-center justify-center py-4'>
+                    <div className='lg:w-44 w-36'>
+                        <img className='w-full object-cover' src={logo12} alt="" />
+                        </div>
+                    </div>
+                    
+                    </div>
+                </Marquee>
             </div>
         </>
     );
